91abaf07a409d52c217a7fe6ecac75546b873603
Bgarcia117 <bryant.garcia.1771@gmail.com>
2023-04-14 08:56:46 -0500

    Created function that compresses files and linked it to the gui.

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import PySimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 # This usese a different type a button to access files
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output")
 
 label2 = sg.Text("Select destination file:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder", text_color="green")
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_button1],
                            [label2, input2, choose_button2],
-                           [compress_button]])
+                           [compress_button, output_label]])
 
-window.read()
+while True:
+        event, values = window.read()
+        print(event, values)
+        filepaths = values["files"].split(",")
+        # since there can be multiple filepaths, they need to split apart
+        folder = values["folder"]
+        make_archive(filepaths, folder)
+        window["output"].update(value="Compression completed!")
+
 window.close()
